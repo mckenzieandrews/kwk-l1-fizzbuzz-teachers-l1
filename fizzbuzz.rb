9a557d371c3108e3a62c29c6882f12710375cad4
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-06-19 03:15:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
+def fizzbuzz(numb)
+  if numb % 3 == 0 && numb % 5 == 0
     "FizzBuzz"
-  elsif number % 3 == 0
+  elsif numb % 3 == 0
     "Fizz"
-  elsif number % 5 == 0
+  elsif numb % 5 == 0
     "Buzz"
   else
   end
